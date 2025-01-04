4e26a1ce600b33f4095fa737f1693a79a52c6c0d
zarjin-404 <zarjinislamjewel@gmail.com>
2025-01-04 14:02:48 +0600

    refactor user model and update CRUD endpoints for consistency

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-const userModel = require('./user.models');
+const User = require('./user.models');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Mongo Data Base');
 });
 
-//create User
-app.get('/create', async (req, res) => {
-  const user = await userModel.create({
-    name: 'Zarjin Islam',
-    username: 'zarjin',
-    email: 'zarjin@gmail.com',
-    password: '12345699999',
+app.get('/createUser', async (req, res) => {
+  const user = await User.create({
+    fullName: 'Jewel Rana',
+    userName: 'jewel',
+    email: 'jewelrana@gamil.com',
+    password: '12345678',
   });
   res.send(user);
 });
 
-//Update User
-app.get('/update', async (req, res) => {
-  const userUpdate = await userModel.findOneAndUpdate(
-    {
-      username: 'johndoe',
-    },
-    { name: 'Zarjin Islam Jewel' },
-    { new: true },
-  );
-  res.send(userUpdate);
+app.get('/readUser', async (req, res) => {
+  const user = await User.find();
+  res.send(user);
 });
 
-//read User
-app.get('/read', async (req, res) => {
-  const usersRead = await userModel.find();
-  res.send(usersRead);
+app.get('/upadataUser', async (req, res) => {
+  const user = await User.findOneAndUpdate({ userName: 'jewel' }, { fullName: 'Ayman Islam' }, { new: true });
+  res.send(user);
 });
 
-//delete User
-app.get('/delete', async (req, res) => {
-  const usersDelete = await userModel.findOneAndDelete({ username: 'zarjin' });
-  res.send(usersDelete);
+app.get('/deleteUser', async (req, res) => {
+  const user = await User.findOneAndDelete({ userName: 'jewel' });
+  res.send(user);
 });
 
 app.listen(port, () => {
--- user.models.js
+++ user.models.js
@@ -1,11 +1,9 @@
-const e = require('express');
 const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb://127.0.0.1:27017/mongooestest`);
+mongoose.connect('mongodb://127.0.0.1:27017/testMongoose');
 
 const userSchema = mongoose.Schema({
-  name: String,
-  username: String,
+  fullName: String,
+  userName: String,
   email: String,
   password: String,
 });
